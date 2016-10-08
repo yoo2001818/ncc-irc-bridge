5b14980b185d99115cea3b0650fd25d473e6afc2
Duknam Yoo <yoo2001818@gmail.com>
2016-10-08 17:00:28 +0900

    Add discord retrying

--- src/transport/discord.js
+++ src/transport/discord.js
@@ -3,15 +3,12 @@
 const Transport = require('./transport.js');
 
 class IRCTransport extends Transport {
-  notifyUser(type, from, to, message, additionalProps) {
+  notifyUser(type, user, to, message, additionalProps) {
     this.notify(Object.assign({
       type: type,
       room: to,
       time: new Date(),
-      user: {
-        id: from,
-        nickname: from
-      },
+      user: user,
       message: message,
       sent: false
     }, additionalProps || {}));
@@ -24,21 +21,28 @@ class IRCTransport extends Transport {
       if (userID === connection.id) return;
       let attachments = event.d.attachments;
       if (attachments.length >= 1) {
-        this.notifyUser('image', user, channelID, message + ': ' +
-          attachments[0].url);
+        this.notifyUser('image', {
+          id: userID,
+          nickname: user
+        }, channelID, message ? (message + ': ') : '' + attachments[0].url);
         return;
       }
-      this.notifyUser('text', user, channelID, message);
+      this.notifyUser('text', {
+        id: userID,
+        nickname: user
+      }, channelID, message);
     });
     this.rooms = {};
   }
   join(room) {
     // this.connection.join(room);
   }
-  send(room, message) {
+  send(room, message, retries = 0) {
     this.connection.sendMessage({
       to: room,
       message
+    }, (error) => {
+      if (error && retries < 3) this.send(room, message, retries + 1);
     });
   }
 }
